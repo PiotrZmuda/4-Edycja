09fbca0ab55078fe8ab9886e867e1f4975804c2e
PiotrZmuda <piotrzmuda07@gmail.com>
2023-03-07 20:45:44 +0100

    changes

---
+++ test.js
@@ -0,0 +1,15 @@
+
+// 4. ** Mamy listę pracowników i chcemy sprawdzić czy dany pracownik,np. “Gordon
+// Sheridon”, jest umieszczony w tej tablicy. Ponadto, jeżeli jego imię i nazwisko
+// figuruje w spisie, to chcemy aby została wyświetlona informacja o indexie elementu,
+// w którym została znaleziona osoba - ułatwi to jej odszukanie w spisie.
+// const pracownicy = [
+// { name: 'Jake Bale', age: 30 },
+// { name: 'Adam Smith', age: 23 },
+// { name: 'Gordon Sheridon', age: 56 },
+// { name: 'Valley Balley', age: 78 }
+// ];
+// Prawdopodobnie przyda Ci się wykorzystanie metody string.indexOf: 
+// https://developer.mozilla.org/enUS/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+// console.log z tego zadania będzie wyglądać następująco : true, 2
+
